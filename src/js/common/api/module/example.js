93ccb2bd5b86516ba5dc5b4293bc19801360209e
Evan M Rose <evanmrose@gmail.com>
2019-01-29 14:13:58 -0500

    Setting up boilerplate

--- src/js/common/api/module/example.js
+++ src/js/common/api/module/example.js
@@ -1,6 +1,8 @@
 /*
 // This is an example will not work
 // Just only show case how I would orangize
+// CAT FACTS API - https://alexwohlbruck.github.io/cat-facts/
+// CAT PICS API - https://aws.random.cat/meow
 const API_ENDPOINT = __CONFIG__.apiUrl.avengers
 
 export const getTopTenAvengers = (query = {}) => {
